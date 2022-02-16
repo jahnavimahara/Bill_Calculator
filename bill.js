2e8fcf7bba7a64dd1cc068a5609f93006b70ea9f
Jahnavi Mahara <73386331+jahnavimahara@users.noreply.github.com>
2022-02-16 21:29:43 +0530

    Delete bill.js

--- bill.js
+++
@@ -1,6 +0,0 @@
-function calculateTotalPrice(){
-    var pr=document.getElementById("price").value;
-    var qnty=document.getElementById("qty").value;
-    document.getElementById("totalprice").innerHTML=(parseInt(pr)*parseInt(qnty));
-    return false;
-}
